5445ee653fcb19fcfe9a47fbf7dcbbe5679d3155
CubeHunt3R <apurger@student.tgm.ac.at>
2015-12-17 10:52:46 +0100

    v2

--- src/purger/a14/A14_HelloWorld.java
+++ src/purger/a14/A14_HelloWorld.java
@@ -3,7 +3,9 @@ package purger.a14;
 public class A14_HelloWorld {
 
 	public void HelloWorld(){
-		System.out.println("Hello World");
+		System.out.println("Hello World1");
+		System.out.println("Hello World2");
+		System.out.println("Hello World3");
 	}
 	
 	public void HelloWorld_2(){
