b2b4d52d19b1c5f277390aab1e160ef21a73acc1
CubeHunt3R <apurger@student.tgm.ac.at>
2015-12-17 10:50:55 +0100

    HelloWorld + Main commiten

---
+++ src/purger/a14/A14_HelloWorld.java
@@ -0,0 +1,12 @@
+package purger.a14;
+
+public class A14_HelloWorld {
+
+	public void HelloWorld(){
+		System.out.println("Hello World");
+	}
+	
+	public void HelloWorld_2(){
+		System.out.println("Hello World_2");
+	}
+}
---
+++ src/purger/a14/A14_TheMain.java
@@ -0,0 +1,9 @@
+package purger.a14;
+
+public class A14_TheMain {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
